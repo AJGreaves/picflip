8e312f73fdb459c2af90b6df8e85b816c6835885
AGreaves <gilhespy.anna@gmail.com>
2019-03-18 16:26:57 +0000

    audio for card flips added

--- js/my_script.js
+++ js/my_script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     
     // flips game card on click
     $('.flip-card').click(function() {
-    $(this).toggleClass('active');
+        $(this).toggleClass('active');
+        $(this).find('audio')[0].play()
     });
-    
 })

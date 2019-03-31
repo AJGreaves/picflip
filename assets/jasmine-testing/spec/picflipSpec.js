04030593a81d8e5160d2446f8cea5f85d0f86895
AGreaves <gilhespy.anna@gmail.com>
2019-03-31 18:45:45 +0000

    howManyCards function tests written and working in Jasmine

--- assets/jasmine-testing/spec/picflipSpec.js
+++ assets/jasmine-testing/spec/picflipSpec.js
@@ -5,35 +5,21 @@ describe('turnsCounter function', function() {
     });
     
     it('should return true when countSelected = 0', function() {
-        // arrange
-        
-        //act
         let result = checkCounter(); 
-        
-        //assert
         expect(result).toBe(true);
     
     });
     
     it('should return true when countSelected = 1', function() {
-        // arrange
         countSelected = 1; 
-        //act
         let result = checkCounter(); 
-        
-        //assert
         expect(result).toBe(true);
     
     });
     
         it('should return false when countSelected = 2', function() {
-        // arrange
         countSelected = 2; 
-        
-        //act
         let result = checkCounter(); 
-        
-        //assert
         expect(result).toBe(false);
     
     });
@@ -46,41 +32,51 @@ describe('muteAudio function', function() {
     });
     
     it('should return silence = false', function(){
-        //arrange
-        
-        //act
         let result = muteAudio();
-        
-        //assert
         expect(silence).toBe(false);
         
     });
     
         it('should return silence = true', function(){
-        //arrange
         silence = false;
-        //act
         let result = muteAudio();
-        
-        //assert
         expect(silence).toBe(true);
         
     });
 });
 
 describe('howManyCards function', function() {
-    
-    beforeEach(() => {
-        num = 0;
+
+    it('should return 4', function() {
+        setFixtures(`
+			<div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+            <div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+			`)
+        let result = howManyCards();
+        expect(result).toEqual(4);
+
     });
     
-    it('should return 4', function() {
-        //arrange
-        num = 8;
-        //act
-        let result = howManyCards(num);
-        //assert
-        expect(result).toBe(4);
-        
+    it('should return 6', function() {
+        setFixtures(`
+			<div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+            <div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+            <div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+			`)
+        let result = howManyCards();
+        expect(result).toEqual(6);
+
+    });
+    
+    it('should return 8', function() {
+        setFixtures(`
+			<div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+            <div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+            <div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+            <div class="visible"></div><div class="visible"></div><div class="visible"></div><div class="visible"></div>
+		`)
+        let result = howManyCards();
+        expect(result).toEqual(8);
     });
 });
+
